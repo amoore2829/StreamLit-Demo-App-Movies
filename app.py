66de030aeaab19c04245d910f1505d37ff999a06
Alex Moore <99160263+amoore2829@users.noreply.github.com>
2023-05-25 14:53:23 -0400

    optimized code for faster runtime

--- app.py
+++ app.py
@@ -54,32 +54,46 @@ genre_colors = {genre: f"#{random.randint(0, 0xFFFFFF):06x}" for genre in genre_
 # Create an interactive chart based on user selection
 chart_type = st.sidebar.radio("Select Genre Chart Type", options=["Horizontal Bar Chart", "Pie Chart"])
 
+# Create figure and axes objects outside the condition
+fig, ax = plt.subplots(figsize=(10, 6))
+
 if chart_type == "Horizontal Bar Chart":
     # Create a horizontal bar chart using Matplotlib
-    plt.figure(figsize=(10, 6))
-    bars = plt.barh(genre_counts['Genre'], genre_counts['Count'], color=[genre_colors[genre] for genre in genre_counts['Genre']])
-    plt.xlabel('Count')
-    plt.ylabel('Genre')
-    plt.title('Movie Genre Counts')
+    bars = ax.barh(genre_counts['Genre'], genre_counts['Count'], color=[genre_colors[genre] for genre in genre_counts['Genre']])
+    ax.set_xlabel('Count')
+    ax.set_ylabel('Genre')
+    ax.set_title('Movie Genre Counts')
 
     # Add legend
-    plt.legend(bars, genre_counts['Genre'])
+    ax.legend(bars, genre_counts['Genre'])
     plt.tight_layout()
-    st.pyplot(plt)
+
+    # Display the chart in a container with a maximum width
+    with st.container():
+        col1, col2 = st.columns([1, 4])
+        with col1:
+            st.write('')
+        with col2:
+            st.pyplot(fig)
 
 else:
     # Create a pie chart using Matplotlib
-    plt.figure(figsize=(6, 6))
-    wedges, texts, autotexts = plt.pie(genre_counts['Count'], labels=genre_counts['Genre'], colors=[genre_colors[genre] for genre in genre_counts['Genre']], autopct='%1.1f%%')
-    plt.title('Movie Genre Distribution')
+    wedges, texts, autotexts = ax.pie(genre_counts['Count'], labels=genre_counts['Genre'], colors=[genre_colors[genre] for genre in genre_counts['Genre']], autopct='%1.1f%%')
+    ax.set_title('Movie Genre Distribution')
 
     # Add legend without overlapping labels
-    plt.legend(wedges, genre_counts['Genre'], loc='center left', bbox_to_anchor=(1, 0.5), title='Genre')
+    ax.legend(wedges, genre_counts['Genre'], loc='center left', bbox_to_anchor=(1, 0.5), title='Genre')
 
     # Adjust label positions to avoid overlap
     plt.subplots_adjust(left=0.0, bottom=0.1, right=0.55)
 
-    st.pyplot(plt)
+    # Display the chart in a container with a maximum width
+    with st.container():
+        col1, col2 = st.columns([1, 4])
+        with col1:
+            st.write('')
+        with col2:
+            st.pyplot(fig)
 
 st.markdown('## Movie Title Comparison')
 
@@ -90,23 +104,38 @@ search_results = df[df['title'].str.contains(search_query, case=False)]
 # Select chart type for movie title comparison
 title_chart_type = st.sidebar.radio("Select Title Chart Type", options=["Horizontal Bar Chart", "Pie Chart"])
 
+# Create figure and axes objects outside the condition
+fig, ax = plt.subplots(figsize=(10, 6))
+
 # Display horizontal bar chart of selected movie titles
 if not search_results.empty and title_chart_type == "Horizontal Bar Chart":
-    plt.figure(figsize=(10, 6))
-    bars = plt.barh(search_results['title'], search_results['popularity'], color='dodgerblue')
-    plt.xlabel('Popularity')
-    plt.ylabel('Movie Title')
-    plt.title('Movie Title Popularity Comparison')
+    bars = ax.barh(search_results['title'], search_results['popularity'], color='dodgerblue')
+    ax.set_xlabel('Popularity')
+    ax.set_ylabel('Movie Title')
+    ax.set_title('Movie Title Popularity Comparison')
     plt.tight_layout()
-    st.pyplot(plt)
+
+    # Display the chart in a container with a maximum width
+    with st.container():
+        col1, col2 = st.columns([1, 4])
+        with col1:
+            st.write('')
+        with col2:
+            st.pyplot(fig)
 
 # Display pie chart of selected movie titles
 elif not search_results.empty and title_chart_type == "Pie Chart":
-    plt.figure(figsize=(6, 6))
-    wedges, texts, autotexts = plt.pie(search_results['popularity'], labels=search_results['title'], autopct='%1.1f%%')
-    plt.title('Movie Title Popularity Distribution')
+    wedges, texts, autotexts = ax.pie(search_results['popularity'], labels=search_results['title'], autopct='%1.1f%%')
+    ax.set_title('Movie Title Popularity Distribution')
     plt.subplots_adjust(left=0.0, bottom=0.1, right=0.55)
-    st.pyplot(plt)
+
+    # Display the chart in a container with a maximum width
+    with st.container():
+        col1, col2 = st.columns([1, 4])
+        with col1:
+            st.write('')
+        with col2:
+            st.pyplot(fig)
 
 elif search_results.empty:
     st.markdown('No matching movie titles found.')
